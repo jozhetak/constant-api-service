d9b373784cda02e75f1c57ae92cb95034d2de507
0xsirrush <thaibao@autonomous.nyc>
2018-11-27 15:16:31 +0700

    - update

--- service/walletsservice.go
+++ service/walletsservice.go
@@ -85,10 +85,10 @@ func (w *WalletService) Send(privKey string, req serializers.WalletSend) error {
 	switch req.Type {
 	case 0:
 		// send coin constant
-		_, err = w.bc.Createandsendtransaction(privKey, req)
+		_, err = w.bc.CreateAndSendConstantTransaction(privKey, req)
 	case 1:
 		// send coin constant
-		err = w.bc.Sendcustomtokentransaction(privKey, req)
+		err = w.bc.SendCustomTokenTransaction(privKey, req)
 	}
 	return err
 }
