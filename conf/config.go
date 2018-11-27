fc11070c7bf0af425cc4042f0690992f30cae14e
Trần Thành <stone@Trans-MacBook-Pro.local>
2018-11-27 16:30:25 +0700

    add more conf (1)

--- conf/config.go
+++ conf/config.go
@@ -29,8 +29,12 @@ func GetConfig() *Config {
 }
 
 type Config struct {
-	Port           int    `json:"port"`
-	Db             string `json:"db"`
-	TokenSecretKey string `json:"token_secret_key"`
-	SendgridAPIKey string `json:"sendgrid_api_key"`
+	Port           		int    `json:"port"`
+	Db             		string `json:"db"`
+	ExchangeObEndpoint	string `json:"exchange_ob_endpoint"`
+	EthChain			string `json:"eth_chain"`
+	SimpleLoanAddress	string `json:"simpleloan_address"`
+	SimpleLoanOwner		string `json:"simpleloan_owner"`
+	TokenSecretKey		string `json:"token_secret_key"`
+	SendgridAPIKey		string `json:"sendgrid_api_key"`
 }
