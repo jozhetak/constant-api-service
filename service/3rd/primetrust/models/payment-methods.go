94b0113440ca6b9a57978afe596ae27b1c088f82
hieuqautonomous <hieu.q@autonomous.nyc>
2018-11-28 11:33:58 +0700

    primetrust 3rd

--- service/3rd/primetrust/models/payment-methods.go
+++ service/3rd/primetrust/models/payment-methods.go
@@ -16,26 +16,26 @@ const (
 )
 
 type PaymentMethodAttributes struct {
-	ContactID                 string  `json:"contact-id"`
-	AchCheckType              string  `json:"ach-check-type"` // (enum): personal or business.
-	BankAccountName           string  `json:"bank-account-name"`
-	BankAccountType           string  `json:"bank-account-type"`
-	BankAccountNumber         string  `json:"bank-account-number"`
-	BankName                  *string `json:"bank-name"`
-	CheckPayee                *string `json:"check-payee"`
-	ContactEmail              string  `json:"contact-email"`
-	ContactName               string  `json:"contact-name"`
-	CreditCardName            *string `json:"credit-card-name"`
-	CreditCardPostalCode      *string `json:"credit-card-postal-code"`
-	CreditCardType            *string `json:"credit-card-type"`
-	Inactive                  bool    `json:"inactive"`
-	IntermediaryBankName      *string `json:"intermediary-bank-name"`
-	IntermediaryBankReference *string `json:"intermediary-bank-reference"`
-	Last4                     string  `json:"last-4"`
-	PaymentType               string  `json:"payment-type"` // (enum, sql:true): ach, check, credit_card, wire, or wire_international
-	RoutingNumber             string  `json:"routing-number"`
-	SwiftCode                 *string `json:"swift-code"`
-	IpAddress                 string  `json:"ip-address"`
+	ContactID                 string `json:"contact-id"`
+	AchCheckType              string `json:"ach-check-type"` // (enum): personal or business.
+	BankAccountName           string `json:"bank-account-name"`
+	BankAccountType           string `json:"bank-account-type"`
+	BankAccountNumber         string `json:"bank-account-number"`
+	BankName                  string `json:"bank-name"`
+	CheckPayee                string `json:"check-payee"`
+	ContactEmail              string `json:"contact-email"`
+	ContactName               string `json:"contact-name"`
+	CreditCardName            string `json:"credit-card-name"`
+	CreditCardPostalCode      string `json:"credit-card-postal-code"`
+	CreditCardType            string `json:"credit-card-type"`
+	Inactive                  bool   `json:"inactive"`
+	IntermediaryBankName      string `json:"intermediary-bank-name"`
+	IntermediaryBankReference string `json:"intermediary-bank-reference"`
+	Last4                     string `json:"last-4"`
+	PaymentType               string `json:"payment-type"` // (enum, sql:true): ach, check, credit_card, wire, or wire_international
+	RoutingNumber             string `json:"routing-number"`
+	SwiftCode                 string `json:"swift-code"`
+	IpAddress                 string `json:"ip-address"`
 }
 
 type PaymentMethodData struct {
