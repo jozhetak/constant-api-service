e035ebe8ca8647906ad38a8d6564423a5fd54945
hieuqautonomous <hieu.q@autonomous.nyc>
2018-11-29 15:28:16 +0700

    primetrust 3rd

--- service/3rd/primetrust/contributions.go
+++ service/3rd/primetrust/contributions.go
@@ -70,3 +70,22 @@ func GetContributions() (*models.ContributionsResponse, error) {
 
 	return &response, nil
 }
+
+func AuthorizeContribution(contributionId string) (error) {
+	apiUrl := fmt.Sprintf("%s/contributions/%s/sandbox/authorize", _apiPrefix, contributionId)
+	req, err := http.NewRequest("POST", apiUrl, nil)
+	req.Header.Add("Authorization", _authHeader)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New(res.Status)
+	}
+
+	return nil
+}
--- service/3rd/primetrust/disbursements.go
+++ service/3rd/primetrust/disbursements.go
@@ -119,3 +119,23 @@ func DeleteDisbursement(disbursementId string) (error) {
 
 	return nil
 }
+
+func AuthorizeDisbursement(disbursementId string) (error) {
+	apiUrl := fmt.Sprintf("%s/disbursements/%s/sandbox/authorize", _apiPrefix, disbursementId)
+	req, err := http.NewRequest("POST", apiUrl, nil)
+	req.Header.Add("Authorization", _authHeader)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New(res.Status)
+	}
+
+	return nil
+}
+
